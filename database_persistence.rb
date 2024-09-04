dca4db3bd12d2a34c85eafabf9c18aa6f66aada1
William <william.seipp@gmail.com>
2024-09-04 15:50:09 -0700

    use logger instance to output query info instead of puts

--- database_persistence.rb
+++ database_persistence.rb
@@ -1,12 +1,13 @@
 require "pg"
 
 class DatabasePersistence
-  def initialize
+  def initialize(logger)
     @db = PG.connect(dbname: "todos")
+    @logger = logger
   end
 
   def query(statement, *params)
-    puts "#{statement}: #{params}"
+    @logger.info "#{statement}: #{params}"
     @db.exec_params(statement, params)
   end
 
