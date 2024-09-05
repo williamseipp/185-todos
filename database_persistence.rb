8d98b6aaec7f775636e710d384b257983bb3682a
William <william.seipp@gmail.com>
2024-09-04 22:24:43 -0700

    move todo finding feature into private method

--- database_persistence.rb
+++ database_persistence.rb
@@ -16,7 +16,10 @@ class DatabasePersistence
     result = query(sql, id)
 
     tuple = result.first
-    { id: tuple["id"], name: tuple["name"], todos: [] }
+
+    list_id = tuple["id"].to_i
+    todos = find_todos_for_list(list_id)
+    { id: list_id, name: tuple["name"], todos: todos }
   end
 
   def all_lists
@@ -25,15 +28,7 @@ class DatabasePersistence
 
     result.map do |tuple|
       list_id = tuple["id"].to_i
-      todo_sql = "select * from todos where list_id = $1"
-      todos_result = query(todo_sql, list_id)
-
-      todos = todos_result.map do |todo_tuple|
-        { id: todo_tuple["id"].to_i,
-          name: todo_tuple["name"],
-          completed: todo_tuple["completed"] == "t" }
-      end
-
+      todos = find_todos_for_list(list_id)
       { id: list_id, name: tuple["name"], todos: todos }
     end
   end
@@ -74,5 +69,18 @@ class DatabasePersistence
     # list[:todos].each do |todo|
       # todo[:completed] = true
     # end
+  end
+  
+  private
+
+  def find_todos_for_list(list_id)
+    todo_sql = "SELECT * FROM todos WHERE list_id = $1"
+    todos_result = query(todo_sql, list_id)
+
+    todos_result.map do |todo_tuple|
+      { id: todo_tuple["id"].to_i,
+        name: todo_tuple["name"],
+        completed: todo_tuple["completed"] == "t" }
+    end
   end
 end
